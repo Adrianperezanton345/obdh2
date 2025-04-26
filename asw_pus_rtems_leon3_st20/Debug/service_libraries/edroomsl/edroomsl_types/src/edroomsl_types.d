3a0b4fd16e581e6f71c45a9b8a9720393c8bcd72
atcsol <atcsol@atcsol>
2025-04-26 23:31:57 +0200

    First Commit

---
+++ asw_pus_rtems_leon3_st20/Debug/service_libraries/edroomsl/edroomsl_types/src/edroomsl_types.d
@@ -0,0 +1,12 @@
+service_libraries/edroomsl/edroomsl_types/src/edroomsl_types.o \
+  service_libraries/edroomsl/edroomsl_types/src/edroomsl_types.o:  \
+ ../service_libraries/edroomsl/edroomsl_types/src/edroomsl_types.cpp \
+  ../service_libraries/edroomsl/edroomsl_types/include/public/edroomsl_types.h \
+  ../service_libraries/edroomsl/edroomsl_types/include/edroomsl_types/edroomsl_types.h \
+  /home/atcsol/Downloads/asw_pus_rtems_leon3_st20(1)/asw_pus_rtems_leon3_st20/llsw/config/include/public/basic_types.h
+
+../service_libraries/edroomsl/edroomsl_types/include/public/edroomsl_types.h:
+
+../service_libraries/edroomsl/edroomsl_types/include/edroomsl_types/edroomsl_types.h:
+
+/home/atcsol/Downloads/asw_pus_rtems_leon3_st20(1)/asw_pus_rtems_leon3_st20/llsw/config/include/public/basic_types.h:
---
+++ asw_pus_rtems_leon3_st20/Debug/service_libraries/pus_services/pus_sys_data_pool/src/pus_sys_data_pool.d
@@ -0,0 +1,18 @@
+service_libraries/pus_services/pus_sys_data_pool/src/pus_sys_data_pool.o \
+  service_libraries/pus_services/pus_sys_data_pool/src/pus_sys_data_pool.o:  \
+ ../service_libraries/pus_services/pus_sys_data_pool/src/pus_sys_data_pool.c \
+  /home/atcsol/Downloads/asw_pus_rtems_leon3_st20(1)/asw_pus_rtems_leon3_st20/service_libraries/pus_services/pus_sys_data_pool/include/public/pus_sys_data_pool.h \
+  /home/atcsol/Downloads/asw_pus_rtems_leon3_st20(1)/asw_pus_rtems_leon3_st20/llsw/config/include/public/config.h \
+  /home/atcsol/Downloads/asw_pus_rtems_leon3_st20(1)/asw_pus_rtems_leon3_st20/asw/edroom_glue/include/edroom_glue/edroomdf.h \
+  /home/atcsol/Downloads/asw_pus_rtems_leon3_st20(1)/asw_pus_rtems_leon3_st20/llsw/config/include/public/basic_types.h \
+  /home/atcsol/Downloads/asw_pus_rtems_leon3_st20(1)/asw_pus_rtems_leon3_st20/service_libraries/serialize/include/public/serialize.h
+
+/home/atcsol/Downloads/asw_pus_rtems_leon3_st20(1)/asw_pus_rtems_leon3_st20/service_libraries/pus_services/pus_sys_data_pool/include/public/pus_sys_data_pool.h:
+
+/home/atcsol/Downloads/asw_pus_rtems_leon3_st20(1)/asw_pus_rtems_leon3_st20/llsw/config/include/public/config.h:
+
+/home/atcsol/Downloads/asw_pus_rtems_leon3_st20(1)/asw_pus_rtems_leon3_st20/asw/edroom_glue/include/edroom_glue/edroomdf.h:
+
+/home/atcsol/Downloads/asw_pus_rtems_leon3_st20(1)/asw_pus_rtems_leon3_st20/llsw/config/include/public/basic_types.h:
+
+/home/atcsol/Downloads/asw_pus_rtems_leon3_st20(1)/asw_pus_rtems_leon3_st20/service_libraries/serialize/include/public/serialize.h:
